2079008fd8bf06931682d4a60067883b248fe3d5
Maddi <maddi.fitzmaurice@gmail.com>
2023-05-10 21:45:30 +1000

    Movement Stutter Bug Fix + Billboarding Test
    - Fixed stuttering with camera follow movement
    - Started testing sprite billboarding for NPCs

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class NPC : MonoBehaviour
 {
     [SerializeField] private GameObject _interactUI;
+    private Camera _mainCam;
 
     void Start()
     {
+        _mainCam = Camera.main;
         _interactUI.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0f, _mainCam.transform.rotation.eulerAngles.y, 0f);
     }
 
     public void OnPlayerInteract(Collider other, bool isActive)
